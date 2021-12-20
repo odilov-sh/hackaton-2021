4168038ee22a5985e0ffef78f077b2ebbd29fc00
Dostonbek <otajonov1117@mail.ru>
2021-12-20 19:15:55 +0300

    add_doctor_left

--- frontend/modules/doctor/views/layouts/_left.php
+++ frontend/modules/doctor/views/layouts/_left.php
@@ -3,6 +3,7 @@
 
 $menuItems = [
     ['label' => "Bosh sahifa", 'url' => ['/site/index'], 'icon' => 'home',],
+    ['label' => "Bemorlar", 'url' => ['/doctor/client'], 'icon' => 'user',],
     ['label' => "Qabul", 'url' => ['/doctor/reception'], 'icon' => 'user',],
 
 ];
