aa0b6e1aaaee885d11e0a35bafbf22626b294670
uluGbek <ulugbek@mail.ru>
2021-12-21 09:46:25 +0500

    chart to'g'rilandi

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -7,7 +7,7 @@
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Tizimga kirish';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
