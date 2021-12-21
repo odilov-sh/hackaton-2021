9c34132c76a8909a22d68f2dd2cadda470429e1d
uluGbek <ulugbek@mail.ru>
2021-12-21 11:31:09 +0500

    login page to'g'irlandi

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,19 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'https://fonts.googleapis.com',
+        'https://fonts.gstatic.com',
+        'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@100;300;400;500;700&display=swap',
+        'template/login_page/css/modern-normalize.min.css',
+        'template/login_page/css/bootstrap.min.css',
+        'template/login_page/css/main.css',
+//        'css/site.css',
     ];
     public $js = [
+        'template/login_page/js/background-video.js',
+        'template/login_page/js/gsap.min.js',
+        'template/login_page/js/main.js',
+
     ];
     public $depends = [
         'frontend\assets\Fa5Asset',
--- frontend/modules/doctor/views/layouts/_footer.php
+++ frontend/modules/doctor/views/layouts/_footer.php
@@ -2,6 +2,6 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 2.0
     </div>
-    <strong>Copyright &copy; 2020-<?= date('Y') ?> <a href="https://isoftware.uz">iTeach Software Group</a>.</strong>
+    <strong>Copyright &copy; 2020-<?= date('Y') ?> <a href="https://isoftware.uz">ISoftware Group</a>.</strong>
     All rights reserved.
 </footer>
